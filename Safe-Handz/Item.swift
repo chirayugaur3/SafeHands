c5de3e11e91dc8acc1945c2f47f924e07eceba11
chirayugaur <2201360010@krmu.edu.in>
2026-03-13 07:16:22 +0530

    Initial Commit

---
+++ Safe-Handz/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Safe-Handz
+//
+//  Created by chirayu gaur on 10/03/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Safe-HandzTests/Safe_HandzTests.swift
@@ -0,0 +1,17 @@
+//
+//  Safe_HandzTests.swift
+//  Safe-HandzTests
+//
+//  Created by chirayu gaur on 10/03/26.
+//
+
+import Testing
+@testable import Safe_Handz
+
+struct Safe_HandzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
